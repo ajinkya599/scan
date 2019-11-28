0e30b3feda1d1115cc1a12074d8ee464bf3c3a29
Ajinkya <aj@microsoft.com>
2019-11-28 11:57:03 +0530

    Invoke klar

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import * as core from '@actions/core';
-const download = require('download');
 import * as fs from 'fs';
+import { ToolRunner } from "@actions/exec/lib/toolrunner";
+import { ExecOptions } from '@actions/exec/lib/interfaces';
+import * as Stream from 'stream';
+const download = require('download');
 
 async function downloadKlar() {
     const klarUrl = "https://github.com/optiopay/klar/releases/download/v2.4.0/klar-2.4.0-linux-amd64";
@@ -16,6 +19,45 @@ async function run() {
   try {
     const klarDownloadPath = await downloadKlar();
     console.log("klarDownloadPath: ", klarDownloadPath);
+    const imageToScan = core.getInput('image-to-scan');
+    console.log("Scanning image: ", imageToScan);
+    let error = "";
+    let output = "";
+    let stdErrStream = new Stream.Writable();
+    let stdOutStream = new Stream.Writable();
+
+    stdErrStream.on('data', (data: string) => {
+      error += data;
+    });
+
+    stdOutStream.on('data', (data: string) => {
+      output += data;
+    });
+
+    const options: ExecOptions = {
+      env: {
+        'CLAIR_ADDR': 'localhost:6060'
+      },
+      errStream: stdErrStream,
+      outStream: stdOutStream,
+      ignoreReturnCode: true
+    };
+
+    const toolRunner = new ToolRunner(klarDownloadPath, [ imageToScan ], options);
+    const code = await toolRunner.exec();
+    
+    if (code == 0) {
+      console.log('Found 0 vulnerabilities');
+    }
+    else if (code == 1) {
+      console.log('Found vulnerabilities: ');
+      console.log('stderr: ', error);
+      console.log('stdout: ', output);
+      // throw new Error('Found vulnerabilities');
+    }
+    else {
+      throw new Error('An error occured while scanning the image for vulnerabilities.');
+    }
     // const nameToGreet = core.getInput('who-to-greet');
     // if (nameToGreet == 'Octocat') {
     //     // the Octocat doesn't want to be greeted here!
